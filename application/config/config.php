777602959cecc5f6c8e8f50b8fdaad3316ce0426
nisrinayulia17 <nisyuliaa@gmail.com>
2024-04-26 20:17:20 +0700

    Updated configuration settings

--- application/config/config.php
+++ application/config/config.php
@@ -24,6 +24,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://localhost/wayang_orang_sriwedari/';
+'https://nisrinayulia17.github.io/wos-user/';
 
 /*
 |--------------------------------------------------------------------------
